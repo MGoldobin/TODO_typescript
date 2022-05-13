0bfa12392c561a87fb3e43e3013acd64d78076e5
Mike <mgoldobin@psu.ru>
2022-05-14 02:45:55 +0500

    type theme

---
+++ src/vendor/theme.ts
@@ -0,0 +1,17 @@
+export type theme = {
+	color: string,
+	bgColor: string,
+	todoBgColor: string
+}
+
+export const lightTheme: theme = {
+	color: '#000',
+	bgColor: '#fff',
+	todoBgColor: "#E5E5E5"
+}
+
+export const darkTheme: theme = {
+	color: '#f4f4f4',
+	bgColor: '#373737',
+	todoBgColor: "#282828"
+}
