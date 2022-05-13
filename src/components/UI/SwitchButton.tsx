9ad01b97e4b70eb0623c82e175a2cb7acda8a922
Mike <mgoldobin@psu.ru>
2022-05-14 02:43:31 +0500

    SwitchButton theme component

---
+++ src/components/UI/SwitchButton.tsx
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+import Switch from '@mui/material/Switch'
+import { observer } from 'mobx-react-lite'
+import { useStore } from '../../hooks/useStore'
+
+const StyledSwitchButton = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+`
+
+const SwitchButton = observer(() => {
+	const { themeStore } = useStore()
+
+	return (
+		<StyledSwitchButton>
+			<img src={themeStore.isDarkTheme ? "./moonWhite.png" : "./moonBlack.png"} alt="Dark mode" />
+			<Switch 
+				checked={themeStore.isDarkTheme}
+				onChange={themeStore.changeTheme} 
+				color="default"
+			/>
+		</StyledSwitchButton>
+	)
+})
+
+export default SwitchButton
