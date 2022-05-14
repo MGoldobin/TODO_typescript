464b849f2ea528c1b80c6dde9b76ab1745c549c5
Mike <mgoldobin@psu.ru>
2022-05-14 23:56:05 +0500

    change rootStore

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,8 +1,10 @@
 import { createContext } from "react"
 import themeStore from "./themeStore"
 import todoStore from "./todoStore"
+import popupStore from "./popupStore"
 
 export const storeContext = createContext({
 	themeStore,
-	todoStore
+	todoStore,
+	popupStore
 })
